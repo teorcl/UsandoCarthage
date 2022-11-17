508459b8144eea59a9a3da4bed2999a8607c3b90
Teodoro <teo.calle.lara@gmail.com>
2022-11-17 15:40:47 -0500

    Initial Commit

---
+++ UsandoCarthage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UsandoCarthage
+//
+//  Created by TEO on 17/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
